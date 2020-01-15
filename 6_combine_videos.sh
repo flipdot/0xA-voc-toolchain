c42aed0d0ab0fd2a1e39f48d2217bad5bd15d428
Soeren Wegener <soeren.wegener@eoda.de>
2020-01-15 23:27:30 +0100

    Fix script to combine cam and screen. End marker need to be done.

--- 6_combine_videos.sh
+++ 6_combine_videos.sh
@@ -2,51 +2,44 @@
 
 . settings.env
 
-# for video_dir in $WORKING_DIR/*/ ;
-# do
-# TODO: Wrong input files, we need to cut them first
-video_dir=$WORKING_DIR/"01_Opening"
+for video_dir in $WORKING_DIR/*/ ;
+do
+
+    if [ ! -f $video_dir/screen_offset.txt ]; then
+        echo "Skipping $video_dir";
+        continue
+    fi
     ffmpeg \
         -hwaccel vaapi \
         -vaapi_device /dev/dri/renderD128 \
-        -i $video_dir/intro.mp4 \
         -i $WORKING_DIR/background.mp4 \
         -i $video_dir/screen_normalized.mkv \
+        -ss $(cat $video_dir/screen_offset.txt) \
         -i $video_dir/cam.mp4 \
         -i $WORKING_DIR/outro.mp4 \
         -filter_complex \
         "
-        [2:v]
+        [1:v]
             scale=1536:864
         [slides];
-        [3:v]
+        [2:v]
             scale=480:270
         [cam];
-        [4:v]
-            fade=
-                t=in:
-                alpha=1,
-            setpts=PTS-STARTPTS+20/TB
-        [outro];
-        [1:v][cam]
+        [0:v][cam]
             overlay=
                x=main_w-overlay_w:
-               y=main_h-overlay_h
+               y=main_h-overlay_h:
+               shortest=1
         [bg_with_cam];
         [bg_with_cam][slides]
             overlay
-        [talk];
-        [0:v][talk]
-            overlay
-        [intro_with_talk];
-        [intro_with_talk][outro]
-            overlay
-        [v]
+        [talk]
         " \
-        -map "[v]" \
-        -map 2:a \
+        -map "[talk]" \
+        -map 1:a \
+        -ss $(cat $video_dir/talk_start_offset.txt) \
         -qp:v $QUALITY \
         -c:a aac \
         -b:a 128k \
-        $video_dir/result.mp4
-# done
+        $video_dir/combined.mp4
+done
